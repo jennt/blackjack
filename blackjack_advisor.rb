965be53c9820090e631336da629a89579644ea8f
Jenn Tustin <jenn.tustin@gmail.com>
2017-01-16 23:02:53 -0500

    Start move calculation

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -32,6 +32,26 @@ def aces(card)
   card
 end
 
+def optimal_move?(first_card, second_card, dealer_card)
+
+end
+
+def hard_move(hard, move)
+  hard_move = hard[first_card][dealer_card]
+  puts "You should #{hard_move}"
+end
+
+def soft_move(soft, move)
+  soft_move = soft[cards_value][dealer_card]
+  puts "You should #{soft_move}"
+end
+
+def pair_move(pair, move)
+  pair_move = pair[cards_value][dealer_card]
+  puts "You should #{pair_move}"
+end
+
+
 hard = {
   5 => {
     2 => 'H',
@@ -143,30 +163,30 @@ dealer_card = 0
 cards_value = 0
 card = 0
 
-  print 'Give me your first card: '
-  first_card = gets.chomp
-  first_card = valid_card(first_card)
-  first_card = convert_face_cards(first_card)
-  first_card = aces(first_card)
+print 'Give me your first card: '
+first_card = gets.chomp
+first_card = valid_card(first_card)
+first_card = convert_face_cards(first_card)
+first_card = aces(first_card)
 
-  print 'Give me your second card: '
-  second_card = gets.chomp
-  second_card = valid_card(second_card)
-  second_card = convert_face_cards(second_card)
-  second_card = aces(second_card)
+print 'Give me your second card: '
+second_card = gets.chomp
+second_card = valid_card(second_card)
+second_card = convert_face_cards(second_card)
+second_card = aces(second_card)
 
-  cards_value = first_card.to_i + second_card.to_i
+cards_value = first_card.to_i + second_card.to_i
 
-  print 'Dealer card: '
-  dealer_card = gets.chomp
-  dealer_card = valid_card(dealer_card)
-  dealer_card = convert_face_cards(dealer_card)
-  dealer_card = aces(dealer_card)
+print 'Dealer card: '
+dealer_card = gets.chomp
+dealer_card = valid_card(dealer_card)
+dealer_card = convert_face_cards(dealer_card)
+dealer_card = aces(dealer_card)
 
-  print "first_card: #{first_card}\n"
-  print "second_card: #{second_card}\n"
-  print "dealer_card: #{dealer_card}\n"
-  print "cards_value: #{cards_value}\n"
+print "first_card: #{first_card}\n"
+print "second_card: #{second_card}\n"
+print "dealer_card: #{dealer_card}\n"
+print "cards_value: #{cards_value}\n"
 
 # hard_move = hard[first_card][dealer_card]
 # puts hard_move
